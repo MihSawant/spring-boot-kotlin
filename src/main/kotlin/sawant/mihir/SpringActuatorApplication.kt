131c51c223a1d91f2281a7b2fa4dfd7d26b54a9d
Mihir Sawant <mihirssawant06@gmail.com>
2023-04-18 15:26:59 +0530

    initial commit

---
+++ src/main/kotlin/sawant/mihir/SpringActuatorApplication.kt
@@ -0,0 +1,11 @@
+package sawant.mihir
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringActuatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringActuatorApplication>(*args)
+}
---
+++ src/test/kotlin/sawant/mihir/SpringActuatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package sawant.mihir
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringActuatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
